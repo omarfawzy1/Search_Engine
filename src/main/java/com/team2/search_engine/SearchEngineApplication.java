f37201bd441120a2e4650350d8ee64931eeba3e6
yehiahendy <yayahendy11@gmail.com>
2022-09-01 19:42:04 +0200

    Add swagger

--- src/main/java/com/team2/search_engine/SearchEngineApplication.java
+++ src/main/java/com/team2/search_engine/SearchEngineApplication.java
@@ -2,8 +2,10 @@ package com.team2.search_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SearchEngineApplication {
 
     public static void main(String[] args) {
