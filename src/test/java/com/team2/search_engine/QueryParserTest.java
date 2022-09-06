a7d0c0affd9166b0f2d45eabb4fc03d2e5441c17
yehiahendy <yayahendy11@gmail.com>
2022-09-06 15:50:47 +0200

    Add QueryParserTest

---
+++ src/test/java/com/team2/search_engine/QueryParserTest.java
@@ -0,0 +1,68 @@
+package com.team2.search_engine;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class QueryParserTest {
+    String validSearchQuery = "PO with Code = 20220001";
+    String unValidTypeSearchQuery = "XO with Code = 20220001";
+
+    @Test
+    @DisplayName("should extract valid search type when you enter valid query")
+    void shouldExtractValidSearchType() {
+        //Arrange
+        //Act
+        //Assert
+        //CleanUp
+    }
+
+    @Test
+    @DisplayName("should throw exception when  invalid search type when you enter invalid query")
+    void shouldThrowExceptionWhenInValidSearchType() {
+        //Arrange
+        //Act
+        //assert
+        //CleanUp
+    }
+
+    @Test
+    @DisplayName("should extract valid search filter when you enter valid query")
+    public void shouldExtractValidSearchBy() {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+        //Clean Up
+    }
+
+    @Test
+    @DisplayName("should extract valid search operator when you enter valid query")
+    public void shouldExtractValidSearchOperator() {
+        //Arrange
+        //Act
+        //Assert
+        //Clean Up
+    }
+
+    @Test
+    @DisplayName("should extract valid search code when you enter valid query")
+    public void shouldExtractValidSearchCode() {
+        //Arrange
+        //Act
+        //Assert
+        //Clean Up
+    }
+
+    @Test
+    @DisplayName("should extract all valid search fields when you enter valid query")
+    public void shouldExtractAllValidFields() {
+        //Arrange
+        //Act
+        //Assert
+        //Clean Up
+    }
+
+}
