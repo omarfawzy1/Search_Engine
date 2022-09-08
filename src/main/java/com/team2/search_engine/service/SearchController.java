121b524f84fd7d2b08d5b763a88b449f2aa8607f
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2022-09-08 21:58:50 +0200

    Cross Origin Requests Enabled

--- src/main/java/com/team2/search_engine/service/SearchController.java
+++ src/main/java/com/team2/search_engine/service/SearchController.java
@@ -3,14 +3,12 @@ package com.team2.search_engine.service;
 import com.team2.search_engine.logic.SearchService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/search")
 public class SearchController {
     private SearchService searchService;
@@ -18,14 +16,10 @@ public class SearchController {
     public SearchController(SearchService searchService) {
         this.searchService = searchService;
     }
-
     @GetMapping("")
     public ResponseEntity<List<?>> findResults(@RequestParam String query) {
         List<?> results = searchService.find(query);
         return new ResponseEntity<>(results, HttpStatus.OK);
     }
-    @GetMapping("/list")
-    public String test(){
-        return "test";
-    }
+
 }
